b98843ad310bfe66fda71c783a4f5cfdf6ef2377
Sowjanyappl <sowjanya@peoplelinkvc.com>
2021-01-25 11:08:42 +0530

    Initial Commit

---
+++ KSJFramework/KSJFramework.h
@@ -0,0 +1,18 @@
+//
+//  KSJFramework.h
+//  KSJFramework
+//
+//  Created by Sowjanya Chavala on 25/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KSJFramework.
+FOUNDATION_EXPORT double KSJFrameworkVersionNumber;
+
+//! Project version string for KSJFramework.
+FOUNDATION_EXPORT const unsigned char KSJFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSJFramework/PublicHeader.h>
+
+
